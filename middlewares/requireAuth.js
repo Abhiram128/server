1a6c77630e2fe691d3c2e0481153f6dbe4956ab8
Abhiram128 <90932429+Abhiram128@users.noreply.github.com>
2023-10-06 23:39:30 +0530

    Update requireAuth.js

--- middlewares/requireAuth.js
+++ middlewares/requireAuth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 const requireAuth = async (req, res, next) => {
-  // verify authentication
+  // Verify authentication
   const { authorization } = req.headers;
 
   if (!authorization) {
@@ -12,13 +12,26 @@ const requireAuth = async (req, res, next) => {
   const token = authorization.split(" ")[1];
 
   try {
-    const { id } = jwt.verify(token, process.env.SECRET);
+    const { id, type } = jwt.verify(token, process.env.SECRET);
 
-    req.user = await User.findOne({ _id: id }).select("_id");
+    // Find the user by ID
+    const user = await User.findOne({ _id: id });
+
+    if (!user) {
+      return res.status(401).json({ error: "Token is not authorized" });
+    }
+
+    // Check if the user is verified for email authentication (add your field name accordingly)
+    if (!user.isVerified && type === "emailVerification") {
+      return res.status(401).json({ error: "Email is not verified" });
+    }
+
+    // Attach the user object to the request for further use in routes
+    req.user = user;
 
     next();
   } catch (err) {
-    res.status(401).json({ error: "Token isn't authorized" });
+    res.status(401).json({ error: "Token is not authorized" });
   }
 };
 
